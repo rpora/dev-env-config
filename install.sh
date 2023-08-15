0ca1b4154732ae219ff281006799ad844b07aeb0
Raphael Pora <raphael.pora@gmail.com>
2023-08-15 16:13:17 +0200

    remove ansible

--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-apt update && apt install -y curl wget python3 pipx 
-pipx ensurepath
-pipx install --include-deps ansible
+apt update && apt install -y curl wget python3 pipx build-essentials
