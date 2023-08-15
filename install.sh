202e077142c5b553a3c1776774ccb18528717253
Raphael Pora <raphael.pora@gmail.com>
2023-08-15 16:41:49 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # base
-sudo apt update && apt install -y curl wget unzip python3 pipx build-essentials
+sudo apt update && sudo apt install -y curl wget unzip python3 pipx build-essential
 
 # node.js and nmp via volta
 curl https://get.volta.sh | bash
+export VOLTA_HOME=$HOME/.volta
+export PATH=$VOLTA_HOME/bin:$PATH
 volta install node
 
 # rust
