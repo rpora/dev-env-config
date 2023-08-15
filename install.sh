01ea2f60dda71aed04fdbdeb485b8d2ca0f20630
Raphael Pora <raphael.pora@gmail.com>
2023-08-15 16:27:35 +0200

    node.js and rust

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-apt update && apt install -y curl wget python3 pipx build-essentials
+
+# base
+sudo apt update && apt install -y curl wget unzip python3 pipx build-essentials
+
+# node.js and nmp via volta
+curl https://get.volta.sh | bash
+volta install node
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
