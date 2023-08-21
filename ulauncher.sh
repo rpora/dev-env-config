653771dea14428ee40e3def601d7016186d9db8c
Raphael Pora <raphael.pora@gmail.com>
2023-08-21 11:04:40 +0200

    ulauncher config

---
+++ ulauncher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo add-apt-repository ppa:agornostal/ulauncher
+sudo apt update
+sudo apt install ulauncher
+
+## todo theme for ulauncher
+# copy the .ulaucher folder to ~./config/ulauncher
